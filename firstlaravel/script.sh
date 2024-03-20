f4ccdf61453b01270f7b13db94cc072c2e9aa29c
HotRobloxMale <kacper.kmiecik2003@gmail.com>
2024-03-20 14:42:06 +0100

    fix

--- firstlaravel/script.sh
+++ firstlaravel/script.sh
@@ -3,6 +3,9 @@
 # Start Laravel server in the background
 php artisan serve --host=0.0.0.0 &
 
+npm install vite
+
+npm install laravel-vite-plugin
 # Start Vite for frontend development in the background
 npm run dev &
 
